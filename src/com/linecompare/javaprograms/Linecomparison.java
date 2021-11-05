14b120f1b2b6641cdb317b22cbe0736ab45a7963
Shark1906 <amanraj1906@gmail.com>
2021-11-05 07:00:13 +0530

    ADDED : UC2

--- src/com/linecompare/javaprograms/Linecomparison.java
+++ src/com/linecompare/javaprograms/Linecomparison.java
@@ -8,17 +8,33 @@ public class Linecomparison {
 		
 		System.out.println("Welcome to Line Comparison Computation Program");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter x-cordinate for point(x1, y1) ");
+		System.out.println("Enter x-cordinate of Point1 of Line1(x1, y1)");
 		int x1 = sc.nextInt();
-		System.out.println("Enter y-cordinate for point(x1, y1) ");
+		System.out.println("Enter y-cordinate of Point1 of Line1(x1, y1)");
 		int y1 = sc.nextInt();
-		System.out.println("Enter x-cordinate for point(x2, y2) ");
+		System.out.println("Enter x-cordinate of Point2 of Line1(x2, y2)");
 		int x2 = sc.nextInt();
-		System.out.println("Enter y-cordinate for point(x2, y2) ");
+		System.out.println("Enter y-cordinate of Point2 of Line1(x2, y2)");
 		int y2 = sc.nextInt();
+		System.out.println("Enter x-cordinate of Point1 of Line2(x3, y3)");
+		int x3 = sc.nextInt();
+		System.out.println("Enter y-cordinate of Point1 of Line2(x3, y3)");
+		int y3 = sc.nextInt();
+		System.out.println("Enter x-cordinate of Point2 of Line2(x4, y4)");
+		int x4 = sc.nextInt();
+		System.out.println("Enter y-cordinate of Point2 of Line2(x4, y4)");
+		int y4 = sc.nextInt();
 		
-		double length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-		System.out.println("Length of Line : " + length);
+		double length1 = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		double length2 = Math.sqrt(Math.pow(x4-x3, 2) + Math.pow(y4-y3, 2));
+		Double line1 = new Double(length1);
+		Double line2 = new Double(length2);
+		if(line1.equals(line2)) {
+			System.out.println("Both Lines are Equal");
+		}
+		else {
+			System.out.println("Both Lines are not Equal");
+		}
 		sc.close();
 	}
 
